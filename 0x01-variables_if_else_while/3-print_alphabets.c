5bf4a308f2fe642d9617b950af5265d8419702cc
SamsonKi <Samsonkitaw@gmail.com>
2022-04-09 00:16:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - THe entry point of the program
+*
+* Return: The main function prints the alphabet
+*/
+int main(void)
+{
+	int ch, sec;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (sec = 'A'; sec <= 'Z'; sec++)
+	{
+		putchar(sec);
+	}
+	putchar('\n');
+	return (0);
+}
