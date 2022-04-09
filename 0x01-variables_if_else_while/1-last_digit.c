388ba37ad1fcb50c51fb28aae201667368a80e2d
SamsonKi <Samsonkitaw@gmail.com>
2022-04-09 00:09:02 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,26 +4,30 @@
 /* more headers goes there */
 
 /**
-* main - this is the main function
+* main - The main function that calls everything
 *
-* Return: return whether the number is +/-
+* Return: If the number is less than 6, greater than 5 or equal to zero
 */
 int main(void)
 {
-/*
-* This is for main
-*
-* Description:
-* This function does whatever
-*/
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
-	printf("%i is negative\n", n);
-else if (n > 0)
-	printf("%i is positive\n", n);
-else
-	printf("%i is zero\n", n);
-return (0);
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	if (last_digit > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",
+		       n, last_digit);
+	}
+	return (0);
 }
