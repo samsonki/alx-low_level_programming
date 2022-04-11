8a360daa6003fa3af22c70978b6ec7f5c4c47d28
SamsonKi <Samsonkitaw@gmail.com>
2022-04-11 12:55:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,14 @@ int main(void)
     {
       for  (digit2 = digit1 + 1; digit2 < 10; digit2++)
 	{
-	  Putchar((digit1 % 10) + '0');
-	  Putchar((digit2 % 10) + '0');
+	  putchar((digit1 % 10) + '0');
+	  putchar((digit2 % 10) + '0');
 	    if (digit1 == 8 && digit2 == 9)
 	      continue;
-	  Putchar(',');
-	  Putchar(' ');
+	  putchar(',');
+	  putchar(' ');
 	    }
     }
-  Putchar('\n');
+  putchar('\n');
   return (0);
 }
