e3747d814120771b8ae1205e5ca53a9e1904b8c2
SamsonKi <Samsonkitaw@gmail.com>
2022-04-11 13:48:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,12 +17,16 @@ putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
+
 if (num1 == 98 && num2 == 99)
 continue;
+
 putchar(',');
 putchar(' ');
 }
 }
+
 putchar('\n');
+
 return (0);
 }
