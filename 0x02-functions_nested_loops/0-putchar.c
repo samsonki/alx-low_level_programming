63eeded561f5df4f79ad38d23761020beb1f7f23
SamsonKi <Samsonkitaw@gmail.com>
2022-04-13 07:12:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point.
@@ -7,15 +7,10 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	char ermias[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++)
+	{ 
+		_putchar('\n');
+		return (0);
 }
