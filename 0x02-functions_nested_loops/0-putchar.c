26aa3ea4a6176dde7d249085746d3a700513fc97
SamsonKi <Samsonkitaw@gmail.com>
2022-04-13 13:11:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-char ermias[] = "_putchar";
-
-int c;
-for (c = 0; c < 8; c++)
-{ 
-_putchar(ermias[c]);
-}
-_putchar('\n');
-return (0);
+	char ermias[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
