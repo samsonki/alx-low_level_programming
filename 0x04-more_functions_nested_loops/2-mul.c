b6ab303036e315ffc6790c1d8c952ea16acee1c8
SamsonKi <Samsonkitaw@gmail.com>
2022-04-14 12:45:23 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * mul - print multiplication a and b
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Description: print multiplication result value.
+ * Return: return multiplication result.
  */
 int mul(int a, int b)
 {
-	
 	return (a * b);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* this prototype has to be declared in a header file */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +11,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif
