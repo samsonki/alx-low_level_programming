d8fd7a06d9922b69a30e60fcf53777978d5551b3
SamsonKi <Samsonkitaw@gmail.com>
2022-04-15 06:11:18 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int x = 0;
-	for (; x <= 9; x++)
+	int n;
+
+	for (n = 48; n <= 57; n++)
 	{
-		if (x == 2|| x == 4)
-		{
+		if (n == 50 || n == 52)
 			continue;
-		}
-		else
-		{
-			_putchar(x + '0')
-		}
+		_putchar(n);
 	}
 	_putchar('\n');
 }
